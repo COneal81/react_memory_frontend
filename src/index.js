c44bcd90f45ed008a9f273e0fe718fadf066858f
github username <github email address>
2020-08-04 15:31:32 -0400

    Imported browser router from react-dom, called Router inside provider

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
@@ -12,8 +13,9 @@ import store from './store.js'
 ReactDOM.render(
   
   <Provider store={store}>
-    
+    <Router>
     <App />
+    </Router>
   </Provider>,
  
   document.getElementById('root')
