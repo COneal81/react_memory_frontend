9427ee372ca47e86b8392002e5e40270f46a7dde
github username <github email address>
2020-08-04 15:43:36 -0400

    Changed from backticks to quotes around fetch url

--- src/actions/fetchCategories.js
+++ src/actions/fetchCategories.js
@@ -2,11 +2,11 @@
 
 export function fetchCategories() {
     return (dispatch) => {
-        fetch(`http://localhost:3001/api/v1/categories`)
+        fetch('http://localhost:3000/api/v1/categories')
         .then(resp => resp.json())
         .then(categories => dispatch({
             type: "FETCH_CATEGORIES",
             payload: categories
-        }))
+        }))  
     }
 }
