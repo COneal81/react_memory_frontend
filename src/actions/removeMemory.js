eb780ed29c7ea1932ed1fbfbeb221449d95765eb
github username <github email address>
2020-08-04 15:48:47 -0400

    Changed backticks to quotes around fetch url

--- src/actions/removeMemory.js
+++ src/actions/removeMemory.js
@@ -1,7 +1,7 @@
 export const removeMemory = (memoryId, categoryId) => {
     
     return (dispatch) => {
-        return fetch(`http://localhost:3001/api/v1/categories/${memoryId}/category/${categoryId}`, {
+        return fetch('http://localhost:3000/api/v1/categories/${memoryId}/category/${categoryId}', {
             method: 'DELETE'
         })
         .then(resp => resp.json())
